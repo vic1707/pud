9595a54edf2aa8ffea254b4b05ba826c10634132
vic1707 <28602203+vic1707@users.noreply.github.com>
2025-12-15 22:12:27 +0100

    basic example

--- pud-macros/src/lib.rs
+++ pud-macros/src/lib.rs
@@ -2,6 +2,7 @@
 #![no_std]
 extern crate alloc;
 mod utils;
+use crate::utils::syn_ident_to_pascal_case;
 
 #[proc_macro_attribute]
 pub fn pud(
@@ -17,7 +18,33 @@ fn expand(
 	args: ::proc_macro::TokenStream,
 	item: ::proc_macro::TokenStream,
 ) -> ::syn::Result<::proc_macro2::TokenStream> {
-	let derive_input: ::syn::DeriveInput = ::syn::parse(item)?;
+	let original_declaration: ::proc_macro2::TokenStream = item.clone().into();
+	let ::syn::ItemStruct {
+		fields,
+		vis,
+		ident,
+		generics,
+		..
+	} = ::syn::parse(item)?;
 
-	Ok(::quote::quote! {})
+	let enum_name = ::quote::format_ident!("{}Pud", ident);
+	let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+	let variants = fields
+		.into_iter()
+		.map(|::syn::Field { ident, ty, .. }| -> syn::Variant {
+			let ident = ident
+				.map(|i| syn_ident_to_pascal_case(&i))
+				.expect("Expected named field");
+
+			::syn::parse_quote! { #ident ( #ty ) }
+		});
+
+	Ok(::quote::quote! {
+		#original_declaration
+
+		#vis enum #enum_name #impl_generics #where_clause {
+			#( #variants ),*
+		}
+	})
 }
