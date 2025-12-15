616751f045aa93aa054ca4dfacf611017ed1037c
vic1707 <28602203+vic1707@users.noreply.github.com>
2025-12-15 14:47:34 +0100

    init

---
+++ pud-macros/src/lib.rs
@@ -0,0 +1,21 @@
+#![doc = include_str!("../README.md")]
+#![no_std]
+
+#[proc_macro_attribute]
+pub fn pud(
+	args: ::proc_macro::TokenStream,
+	item: ::proc_macro::TokenStream,
+) -> ::proc_macro::TokenStream {
+	expand(args, item)
+		.unwrap_or_else(|err| err.to_compile_error())
+		.into()
+}
+
+fn expand(
+	args: ::proc_macro::TokenStream,
+	item: ::proc_macro::TokenStream,
+) -> ::syn::Result<::proc_macro2::TokenStream> {
+	let derive_input: ::syn::DeriveInput = ::syn::parse(item)?;
+
+	Ok(::quote::quote! {})
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+#![doc = include_str!("../README.md")]
+// #![no_implicit_prelude] // TODO: enable?
+#![no_std]
+
+pub use ::pud_macros::pud;
