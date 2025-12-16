f518950e78dea8022017d2c75f3026d4476064c9
vic1707 <28602203+vic1707@users.noreply.github.com>
2025-12-16 19:04:18 +0100

    add field  arguments parsing ecosystem

--- pud-macros/src/field.rs
+++ pud-macros/src/field.rs
@@ -1,6 +1,11 @@
 use crate::syn_ident_to_pascal_case;
+use ::{
+	quote::ToTokens as _,
+	syn::parse::{Parse as _, Parser as _},
+};
 
 pub(crate) struct Field {
+	args: Arguments,
 	ident: ::syn::Ident,
 	ty: ::syn::Type,
 }
@@ -11,15 +16,19 @@ impl TryFrom<::syn::Field> for Field {
 		let Some(ident) = field.ident else {
 			return Err(::syn::Error::new_spanned(field, "Expected a named field."));
 		};
-		let ::syn::Field { ty, .. } = field;
+		let ::syn::Field { ty, attrs, .. } = field;
+		let args = Arguments::try_from(attrs.as_slice())?;
 
-		Ok(Self { ident, ty })
+		Ok(Self { ident, ty, args })
 	}
 }
 
 impl Field {
 	pub(crate) fn variant_ident(&self) -> ::syn::Ident {
-		syn_ident_to_pascal_case(&self.ident)
+		self.args
+			.rename
+			.clone()
+			.unwrap_or_else(|| syn_ident_to_pascal_case(&self.ident))
 	}
 
 	pub(crate) fn to_variant(&self) -> ::syn::Variant {
@@ -35,4 +44,54 @@ impl Field {
 
 		::syn::parse_quote! { Self::#variant_ident ( #ident ) => {target. #ident = #ident;} }
 	}
+}
+
+#[derive(Default)]
+pub(crate) struct Arguments {
+	rename: Option<::syn::Ident>,
+}
+
+impl TryFrom<&[::syn::Attribute]> for Arguments {
+	type Error = ::syn::Error;
+
+	fn try_from(attrs: &[::syn::Attribute]) -> ::syn::Result<Self> {
+		let mut args = Self::default();
+
+		for attr in attrs {
+			if attr.path().is_ident("pud") {
+				for arg in parse_pud_attr.parse2(attr.meta.to_token_stream())? {
+					match arg {}
+				}
+			}
+		}
+
+		Ok(args)
+	}
+}
+
+pub(crate) enum Argument {}
+
+impl ::syn::parse::Parse for Argument {
+	fn parse(input: ::syn::parse::ParseStream) -> ::syn::Result<Self> {
+		use ::alloc::string::ToString as _;
+
+		let ident = input.parse::<::syn::Ident>()?;
+		let arg = match ident.to_string().as_str() {
+			_ => return Err(::syn::Error::new_spanned(ident, "Unknown argument.")),
+		};
+		Ok(arg)
+	}
+}
+
+mod kw {
+	::syn::custom_keyword!(pud);
+}
+
+fn parse_pud_attr(
+	attr_ts: ::syn::parse::ParseStream,
+) -> syn::Result<::syn::punctuated::Punctuated<Argument, syn::Token![,]>> {
+	attr_ts.parse::<kw::pud>()?;
+	let content;
+	syn::parenthesized!(content in attr_ts);
+	content.parse_terminated(Argument::parse, syn::Token![,])
 }
